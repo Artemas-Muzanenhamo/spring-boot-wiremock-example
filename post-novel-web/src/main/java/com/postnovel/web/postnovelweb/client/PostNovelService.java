aa327be75b92d4b157941d7a7a3b4bbc715138a3
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-04-12 02:03:23 +0100

    polish up

--- post-novel-web/src/main/java/com/postnovel/web/postnovelweb/client/PostNovelService.java
+++ post-novel-web/src/main/java/com/postnovel/web/postnovelweb/client/PostNovelService.java
@@ -1,14 +1,42 @@
 package com.postnovel.web.postnovelweb.client;
 
 import com.postnovel.web.postnovelweb.domain.Post;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+import static java.lang.String.format;
+import static java.util.Objects.nonNull;
 
 @Service
 public class PostNovelService {
+    private Integer port;
+    private String host;
+    private String scheme;
+
+    public PostNovelService(
+            @Value("${postnovel.client.port}") Integer port,
+            @Value("${postnovel.client.host}") String host,
+            @Value("${postnovel.client.scheme}") String scheme) {
+        this.port = port;
+        this.host = host;
+        this.scheme = scheme;
+    }
 
     public Post getPostById(int id) {
-        final String uri = String.format("http://localhost:8081/posts/%s", id);
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.newInstance()
+                .scheme(scheme)
+                .host(host)
+                .path(format("/posts/%s", id));
+
+        if (nonNull(port)) {
+            uriBuilder.port(port);
+        }
+
+        URI uri = uriBuilder.build().toUri();
 
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject(uri, Post.class);
